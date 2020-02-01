015aac7d2bb8ceaa924e600cdd6106a87f235ae7
Stevi Hall <career.sh.work@gmail.com>
2020-02-01 19:41:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,7 +7,6 @@ def countdown(number)
  
 while number > 0
  puts "#{number} SECOND(S)!"
-sleep 1.5
    number -= 1
 end
 return "HAPPY NEW YEAR!"
@@ -24,6 +23,12 @@ end
 
 def
 countdown_with_sleep(number) 
+
+while number > 0
+ puts "#{number} SECOND(S)!"
+   number -= 1
+end
+return "HAPPY NEW YEAR!"
 end
 
 
